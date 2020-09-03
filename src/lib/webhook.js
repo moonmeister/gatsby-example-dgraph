8ffb7c3c1d795d256d7734b735c0fca8eb877ae6
Alex Moon <alex.jared.moon@gmail.com>
2020-09-03 12:41:36 -0700

    refactor: remove preview trigger, it's un needed

--- src/lib/webhook.js
+++ src/lib/webhook.js
@@ -1,10 +1,8 @@
 
-const PREVIEW_WEBHOOK = process.env.GATSBY_PREVIEW_WEBHOOK
 const BUILD_WEBHOOK = process.env.GATSBY_BUILD_WEBHOOK
 
-
 export async function triggerRefresh(body = {}) {
-  const gatsbyPreview = fetch(PREVIEW_WEBHOOK, {
+  return fetch(BUILD_WEBHOOK, {
     method: 'POST',
     body: JSON.stringify(body),
     headers: {
@@ -12,17 +10,4 @@ export async function triggerRefresh(body = {}) {
     }
   })
 
-  if (BUILD_WEBHOOK === PREVIEW_WEBHOOK) {
-    return
-  }
-
-  const gatsbyBuild = fetch(PREVIEW_WEBHOOK, {
-    method: 'POST',
-    body: JSON.stringify(body),
-    headers: {
-      "Content-Type": "application/json",
-    }
-  })
-
-  return Promise.allSettled([gatsbyPreview, gatsbyBuild])
 }
