6db6c2d15bc0688e9711aed592800556510b4326
Alex Moon <alex.jared.moon@gmail.com>
2020-09-03 16:28:04 -0700

    fix: . in slug

--- src/pages/film/index.js
+++ src/pages/film/index.js
@@ -6,18 +6,24 @@ import Layout from "../../components/Layout"
 export default function FilmPage({ data }) {
   const { allDgraphFilm: { nodes: allFilms } } = data
 
+
   return (
     <Layout explain={"This page may not be edited. This is static data fetched at build time."}>
       <h1>Films</h1>
       <ul>
         {
-          allFilms.map(film => (
-            <li key={film.remoteId}>
-              <article >
-                <Link to={`/film/${slugify(film?.name).toLowerCase()}`}><h2>{film?.name}</h2></Link>
-              </article>
-            </li>
-          ))
+          allFilms.map(film => {
+            const [slug] = slugify(film?.name, {
+              lower: true,
+            }).split('.')
+            return (
+              <li key={film.remoteId}>
+                <article >
+                  <Link to={`/film/${slug}`}><h2>{film?.name}</h2></Link>
+                </article>
+              </li>
+            )
+          })
         }
       </ul>
     </Layout>
